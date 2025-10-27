6046af125fddca7410c64da372959ebe1669144d
Jonah Prout <43648800+jprout2@users.noreply.github.com>
2025-10-27 17:56:34 +0000

    Update Shell.for
    Replaces timeFact with opt_tstep. Adds year_end variable to replace timeFact where it is used for determining if the timestep is at the end of the year. Replaces month with tstep in variable names and column names for output files. Replaces MONTH with TIMESTEP but not sure where this is used.

--- Shell.for
+++ Shell.for
@@ -1,7 +1,7 @@
 C******************************************************************************
 C  Wrapper for RothC model 
 C
-C  August 2025
+C  October 2025
 C
 C  Kevin Coleman
 C  Jonah Prout
@@ -62,9 +62,7 @@ C
       
       integer nsteps
       
-      integer timeFact 
-       
-      integer i, j, k, k_month
+      integer i, j, k, k_tstep
       
       integer t_PC(MAXsteps)
       
@@ -72,7 +70,7 @@ C
       
       integer t_year(MAXsteps)
       
-      integer t_month(MAXsteps)
+      integer t_tstep(MAXsteps)
       
       real*8 t_mod(MAXsteps)
       
@@ -120,7 +118,7 @@ C
       real*8 DPM_Delta, RPM_Delta, Bio_Delta, Hum_Delta, IOM_Delta
       real*8 Total_Delta
       
-      integer YEAR, MONTH
+      integer YEAR, TIMESTEP
       
       integer opt_RMmoist !  1: Standard RothC soil water parameters,
                           !  2: Van Genuchten soil properties and soil is allowed to be drier (ie hygroscopic / capillary water, -1000bar)
@@ -128,6 +126,11 @@ C
       
       integer opt_SMDbare !  1: Standard RothC bareSMD, 
                           !  2: bareSMD is set to wilting point -15bar (could be better for dry soils)
+      
+      integer opt_tstep !  1: Monthly, tstep = 1/12
+                        !  2: Daily, tstep = 1/365
+      
+      integer year_end
       
       real*8 TEMP, RAIN, PEVAP
       
@@ -167,6 +170,7 @@ C
       Bio_Rage = 0.0
       Hum_Rage = 0.0
       IOM_Rage = 50000.0  
+      
 
 C set initial soil water content (deficit) 
       SMD = 0.0
@@ -181,7 +185,7 @@ C read in RothC input data file: data will be passed from other programs at some
 	read(11,*)               ! line is for info only 
 	read(11,*)               ! line is for info only 
 	read(11,*)               ! line is for info only 
-	read(11,*) opt_RMmoist, opt_SMDbare       
+	read(11,*) opt_RMmoist, opt_SMDbare, opt_tstep   
 	read(11,*)               ! line is for info only  
 	read(11,*)               ! line is for info only 
       if (opt_RMmoist.eq.1)then
@@ -193,7 +197,7 @@ C read in RothC input data file: data will be passed from other programs at some
       read(11,*)               ! line is for info only 
 
 	do i = 1, nsteps
-	  read(11,*)t_year(i), t_month(i), t_mod(i), t_tmp(i),t_rain(i),
+	  read(11,*)t_year(i), t_tstep(i), t_mod(i), t_tmp(i),t_rain(i),
      &      t_evap(i), t_C_Inp(i), t_OA_Inp(i), t_PC(i),
      &      t_PL_DPM_f(i), t_PL_RPM_f(i), t_OA_DPM_f(i), t_OA_RPM_f(i),
      &      t_OA_Bio_f(i), t_OA_Hum_f(i)
@@ -203,14 +207,20 @@ C read in RothC input data file: data will be passed from other programs at some
       
       
       open(71, file='year_results.out', status ='unknown')     
-      open(91, file='month_results.out', status ='unknown')
+      open(91, file='tstep_results.out', status ='unknown')
 C
 C READ IN INPUT DATA: END
 C      
       
+      if(opt_tstep == 1)then
+          year_end = 12
+      elseif(opt_tstep == 2)then
+          year_end = 365
+      endif
+      
 C      
 C run RothC to equilibrium: START
-C uses first 12 months of weather and carbon data 
+C uses first year of weather and carbon data 
 C
       k = 0
       j = 0
@@ -219,7 +229,7 @@ C
       SOC = DPM+RPM+Bio+Hum+IOM
       
         write(71,7100)
-7100  format(5x, 'Year,', 2x,  'Month,', 
+7100  format(5x, 'Year,', 2x,  'tstep,', 
      &  1x, 'DPM_t_C_ha,', 1x,  'RPM_t_C_ha,', 
      &  1x, 'Bio_t_C_ha,', 1x, 'Hum_t_C_ha,',
      &  1x, 'IOM_t_C_ha,', 1x, 'SOC_t_C_ha,', 
@@ -227,13 +237,11 @@ C
              
       write(71,101) j, DPM, RPM, Bio, Hum, iom, SOC,total_CO2
 101   format(1x, '       0,', 1x, i6, ',', 7(f11.4, ','), ' -998.02')  
-
-      timeFact = 12 ! monthly
           
       test = 100.0
       
        write(91,9100)
-9100  format(4x, 'Year,',1x,  'Month,',1x, 'C_Inp_t_C_ha,', 
+9100  format(4x, 'Year,',1x,  'tstep,',1x, 'C_Inp_t_C_ha,', 
      &  1x,  'OA_Inp_t_C_ha,', 1x,  'TEMP_C,', 1x, 'RM_TMP,',
      &  1x, 'RAIN_mm,', 1x, 'PEVAP_mm,',1x, 'SMD_mm,',
      &  1x,'RM_Moist,', 1x, 'PC,', 1x,  'RM_PC,',  
@@ -256,7 +264,7 @@ C
        k = k + 1
        j = j + 1 
        
-       if(k.eq.timeFact+1)k = 1   ! 13 if monthly
+       if(k.eq.year_end+1)k = 1   ! 13 if monthly
          if (test < 1E-6) exit
          YEAR = t_year(k)
          TEMP = t_tmp(k)
@@ -278,14 +286,14 @@ C
          
          modernC = t_mod(k) / 100.0             
          
-         call RothC(timeFact, DPM,RPM,Bio,Hum,IOM, SOC, total_CO2, 
+         call RothC(opt_tstep, DPM,RPM,Bio,Hum,IOM, SOC, total_CO2, 
      &     DPM_Rage, RPM_Rage, Bio_Rage, Hum_Rage, Total_Rage, 
      &     modernC, clay, depth,TEMP,RAIN,PEVAP,PC,
      &     PL_DPM_f, PL_RPM_f, OA_DPM_f, OA_RPM_f, OA_Bio_f, OA_Hum_f,
      &     C_Inp, OA_Inp, SMD, RM_TMP, RM_Moist, RM_PC, 
      &     opt_RMmoist, opt_SMDbare, silt, BD, OC, minRM_Moist)  
         
-         if(mod(k, timeFact)== 0)then 
+         if(mod(k, year_end)== 0)then 
            TOC0 = TOC1
            TOC1 = DPM+RPM+Bio+Hum
            test = abs(TOC1-TOC0)            
@@ -316,12 +324,12 @@ C
 C      
 C run RothC for months 13 to the end: START
 C     
-      k_month = 0
-      do i = timeFact+1, nsteps, 1   ! 13 if monthly
+      k_tstep = 0
+      do i = year_end+1, nsteps, 1   ! 13 if monthly
       
-	  k_month = k_month + 1
+	  k_tstep = k_tstep + 1
             
-        if(k_month.eq.timeFact+1)k_month = 1   ! 13 if monthly
+        if(k_tstep.eq.year_end+1)k_tstep = 1   ! 13 if monthly
          
         
          YEAR = t_year(i)
@@ -343,7 +351,7 @@ C
          
          modernC = t_mod(i) / 100.0
            
-         call RothC(timeFact, DPM,RPM,Bio,Hum,IOM, SOC, total_CO2, 
+         call RothC(opt_tstep, DPM,RPM,Bio,Hum,IOM, SOC, total_CO2, 
      &     DPM_Rage, RPM_Rage, Bio_Rage, Hum_Rage, Total_Rage, 
      &     modernC, clay, depth,TEMP,RAIN,PEVAP,PC,
      &     PL_DPM_f, PL_RPM_f, OA_DPM_f, OA_RPM_f, OA_Bio_f, OA_Hum_f,
@@ -353,7 +361,7 @@ C
          Total_Delta = (exp(-Total_Rage/8035.0) - 1.0) * 1000.0
          
          
-         write(91,9103) Year, k_month, C_Inp, OA_Inp, TEMP,RM_TMP, 
+         write(91,9103) Year, k_tstep, C_Inp, OA_Inp, TEMP,RM_TMP, 
      &        RAIN, PEVAP, SMD, RM_Moist, PC, RM_PC,
      &        DPM,RPM,Bio,Hum, IOM, SOC, total_CO2
      
@@ -363,7 +371,7 @@ C
      &        f11.4, ',',f11.4, ',',f11.4, ',',f11.4)        
 
 
-      if(mod(i, timeFact)== 0)then     ! print out results once a year
+      if(mod(i, year_end)== 0)then     ! print out results once a year
         write(71,103) year, DPM, RPM, Bio, Hum, IOM, SOC, total_CO2, 
      &                Total_Delta
       endif
